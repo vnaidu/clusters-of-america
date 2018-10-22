e6436d62ba93cbc72fc9952d70dedb06f372aeb9
Victor Naidu <victor@vnaidu.com>
2018-10-22 13:52:05 -0700

    Merge w/ Local Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clustering cities in the United States by similarty when considering trending venue categories and geographic data.',
+    author='Victor Naidu',
+    license='MIT',
+)
